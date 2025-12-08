9983b8d2fb04744ec0a04192980c3b66b2621f42
TheLsbt <tyresewolmarans@gmail.com>
2025-12-08 19:12:58 +0200

    Built a basic system to test the Array usage

--- in_dev/property.test.gd
+++ in_dev/property.test.gd
@@ -2,61 +2,83 @@
 extends Node
 
 
-var p_joint_count := 0
+@export var advanced_mode := false:
+	set(v):
+		advanced_mode = v
+		notify_property_list_changed()
+
+var _p_item_count := 0
+var _p_items := []
+
+
+# The default template for all items, each entry can have a optional value of is_advanced.
+func get_item_template() -> Array[Dictionary]:
+	return [
+		{
+			"name": "value",
+			"type": TYPE_ARRAY,
+		}
+	]
 
 
 func _get_property_list() -> Array[Dictionary]:
 	var path := ""
 	var props: Array[Dictionary] = []
 	props.append({
-		"name": "joint_count",
+		"name": "_item_count",
 		"type": TYPE_INT,
 		"hint": PROPERTY_HINT_NONE,
 		"hint_string": "",
 		"usage": PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_ARRAY,
-		"class_name": "Joints," + path + "joints/"
+		"class_name": "Items," + path + "items/"
 	})
 
-	for j in 5:
-		var joint_path = "joints/" + str(j) + "/"
-		props.append({
-			"name": joint_path + "value",
-			"type": TYPE_STRING,
-			"usage": PROPERTY_USAGE_DEFAULT
-
-		})
-		props.append({
-			"name": joint_path + "color",
-			"type": TYPE_STRING,
-			"usage": PROPERTY_USAGE_DEFAULT
-
-		})
-
-	#for (uint32_t j = 0; j < settings[i]->joints.size(); j++) {
-			#String joint_path = path + "joints/" + itos(j) + "/";
-			#props.push_back(PropertyInfo(Variant::STRING, joint_path + "bone_name", PROPERTY_HINT_ENUM_SUGGESTION, enum_hint, PROPERTY_USAGE_EDITOR | PROPERTY_USAGE_READ_ONLY));
-			#props.push_back(PropertyInfo(Variant::INT, joint_path + "bone", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_NO_EDITOR | PROPERTY_USAGE_READ_ONLY));
-			#props.push_back(PropertyInfo(Variant::FLOAT, joint_path + "twist_amount", PROPERTY_HINT_RANGE, "0,1,0.001,or_greater,or_less"));
-		#}
+	for j in _p_item_count:
+		var joint_path = path + "items/" + str(j) + "/"
+		for t in get_item_template():
+			t["name"] = joint_path + t.get("name", "")
+			props.append(t)
 
 	return props
-	#, "Joints," + path + "joints/,static,const"));
 
 
 func _get(property: StringName) -> Variant:
-	if property == "joint_count":
-		return p_joint_count
-	elif property.ends_with("value"):
-		return property
+	if property == "_item_count":
+		return _p_item_count
+
+	if property.begins_with("items/"):
+		var index: int = int(property.get_slice("/", 1))
+		var p: String = property.get_slice("/", 2)
+
+		if p == "value":
+			return _p_items[index].get(p, [])
+
+		return _p_items[index].get_or_add(p, null)
+
 	return null
 
 
 func _set(property: StringName, value: Variant) -> bool:
-	if property == "joint_count":
-		p_joint_count = value
-		print(property, " : ", value)
+	if property == "_item_count":
+		_p_item_count = value
+		if _p_item_count < _p_items.size() - 1:
+			_p_items.resize(_p_item_count)
+		else:
+			var a := []
+			a.resize(_p_item_count - _p_items.size())
+			a.fill({})
+			_p_items.append_array(a)
 		notify_property_list_changed()
 		return true
-	print(property, " > ", value)
+
+	if property.begins_with("items/"):
+		var index: int = int(property.get_slice("/", 1))
+		var p: String = property.get_slice("/", 2)
+
+		if p == "value":
+			_p_items[index][p] = value
+			return true
 
 	return false
+
+# TODO: Use _set and _get properly, this is the blueprint to evertything later on.
